5c90265c575e173790710a8e8b1f48119348c920
KhoaPhan <phanthanhkhoa2408@gmail.com>
2026-08-25 23:34:39 +0700

    Create OpenAlexConcept.java

---
+++ backend/src/main/java/com/webnewpaper/backend/client/dto/OpenAlexConcept.java
@@ -0,0 +1,14 @@
+// OpenAlexConcept.java
+package com.webnewpaper.backend.client.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class OpenAlexConcept {
+    @JsonProperty("display_name")
+    private String displayName;
+}
